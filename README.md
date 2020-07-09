96dcf9466d20f8ad6b313544023a5036c918887b
Rod Landaeta <rodrigoelp@gmail.com>
2020-07-09 15:30:43 +1000

    Created the content
    As Paul describes here, trying to align the twitter handle and the account name might be a problem

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # swiftui-customalignment
 Sample project to check how to create custom alignments
+
+
+Following @twostraws tutorial. https://www.youtube.com/watch?v=rxgDWaVaCw4
--- swiftui-customalignment/ContentView.swift
+++ swiftui-customalignment/ContentView.swift
@@ -1,10 +1,41 @@
 //
 
 import SwiftUI
+import FetchImage
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            VStack {
+                Text("@rodrigoelp")
+                ImageView(image: FetchImage(
+                    regularUrl: URL(string: "https://i.pravatar.cc/300")!,
+                    lowDataUrl: URL(string: "https://i.pravatar.cc/100")!))
+                    .frame(width: 64, height: 64)
+                    .clipShape(Circle())
+            }
+
+            VStack {
+                Text("Account")
+                Text("Rod Landaeta")
+                    .font(.largeTitle)
+            }
+        }
+    }
+}
+
+private struct ImageView: View {
+    @ObservedObject var image: FetchImage
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color.gray)
+            image.view?
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+        }.animation(.default)
+            .onAppear(perform: image.fetch)
+            .onDisappear(perform: image.cancel)
     }
 }
 
